323ed0fa4c75ca564d0dc0e5e472ad19213b158c
lcpunch <lcpunchirolli@gmail.com>
2018-10-11 19:07:27 -0400

    This is a test

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
-Route::group(['middleware' => 'auth:api'], function() {
+//Route::group(['middleware' => 'auth:api'], function() {
 
     Route::post('details', 'API\UserController@details');
 
@@ -46,5 +46,5 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::get('calendar', 'API\CalendarController@findAll');
     Route::get('calendar/{id}', 'API\CalendarController@find');
 
-});
+//});
 
