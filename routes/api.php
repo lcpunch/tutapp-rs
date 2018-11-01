2e79288ec15e4ed14f8adabe80c611293c923bb1
umaknow-lucas <lucas@umaknow.com>
2018-10-31 22:51:29 -0400

    cors

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
-//Route::group(['middleware' => 'auth:api'], function() {
+Route::group(['middleware' => 'auth:api'], function() {
 
     Route::post('details/{id}', 'API\UserController@details');
 
@@ -66,4 +66,4 @@ Route::post('register', 'API\UserController@register');
     Route::post('users/import', 'API\UserController@import');
     // Email
     Route::post('/send', 'EmailController@send');
-//});
+});
